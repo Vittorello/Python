601dd0f22a1acff3c2d4685dc01667607b42955a
Igor Camargo Vittorello <igorvittorello@hotmail.com>
2024-11-16 02:34:03 -0300

    Atualizando o código para usar variaveis

--- exercicio_01.py
+++ exercicio_01.py
@@ -1,3 +1,6 @@
 # Crie programa que o usuário digita o seu nome e retorna o número de caracteres
 
-print(len(input("Digite o seu nome: ")))
+nome = input("Digite o seu nome: ")
+qtd_caracteres = len(nome)
+
+print ("O seu nome possui", qtd_caracteres, "caracteres")
